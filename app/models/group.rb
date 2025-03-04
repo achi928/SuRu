64af8cce27e9edc7b429c6133e831c1b916c8c3d
achi928 <192709236+achi928@users.noreply.github.com>
2025-03-04 16:33:10 +0000

    マイグレーションファイル、seedファイル、belongs_to :owner, class_name: 'Member'追記

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :member
   belongs_to :category
+  belongs_to :owner, class_name: 'Member'
   has_many :memberships, dependent: :destroy
   has_many :group_posts, dependent: :destroy
   has_one_attached :group_image
--- db/migrate/20250304113103_create_group_posts.rb
+++ db/migrate/20250304113103_create_group_posts.rb
@@ -2,6 +2,10 @@ class CreateGroupPosts < ActiveRecord::Migration[6.1]
   def change
     create_table :group_posts do |t|
 
+      t.integer :member_id, null: false
+      t.integer :group_id, null: false
+      t.string :content, null: false
+
       t.timestamps
     end
   end
