827672d9604a3c7678f8f9d2cbefae0246b284dc
achi928 <192709236+achi928@users.noreply.github.com>
2025-03-24 07:25:08 +0000

    管理者側機能追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,19 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
+  def index
+    @member = Member.find(params[:member_id])
+    @comments = @member.comments
+  end
+
   def destroy
-    Comment.find_by(id: params[:id], member_id: params[:member_id]).destroy
-    flash[:notice] = 'コメントを削除しました'
-    redirect_to admin_member_path(params[:member_id])
+    comment = Comment.find_by(id: params[:id], member_id: params[:member_id])
+    if comment.destroy
+      flash[:notice] = 'コメントを削除しました'
+    else
+      flash[:alert] = 'コメントの削除に失敗しました'
+    end
+    redirect_to admin_member_comments_path(params[:member_id])
   end
   
 
