516d2e31a3251af19860f73219bbc0243316c520
achi928 <192709236+achi928@users.noreply.github.com>
2025-03-29 15:40:10 +0000

    500エラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :render500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   private
@@ -6,4 +7,7 @@ class ApplicationController < ActionController::Base
     render file: "#{Rails.root}/public/404.html", status: :not_found
   end
 
+  def render500
+    render file: "#{Rails.root}/public/500.html", status: :internal_server_error
+  end
 end
